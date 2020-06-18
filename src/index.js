c3156f4c5bced3e359c15d38cd4ab33cd279e708
John Pham <mr.john.pham@gmail.com>
2020-06-18 11:01:01 -0400

    Remove Matrix class from index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,6 @@
 'use strict';
 
-
-class Matrix {
-    constructor(rows, cols) {
-        this.rows = rows;
-        this.cols = cols;
-        this.data = [];
-
-        for (let r = 0; r < rows; r++) {
-            this.data.push(new Array(cols));
-            for (let c = 0; c < cols; c++) {
-                this.data[r][c] = 0;
-            }
-        }
-
-        //console.log(`The Matrix's data is ${this.data}`);
-    }
-}
+//import Matrix from './matrix.js';
 
 let canvas = new Matrix(3, 4);
 
