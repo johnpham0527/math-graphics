a8310433772966fd0c7ec9c83690257e7c3079af
John Pham <mr.john.pham@gmail.com>
2020-06-18 10:55:11 -0400

    Implement Matrix class

--- math.js
+++ math.js
@@ -1,3 +1,23 @@
 'use strict';
 
+
+class Matrix {
+    constructor(rows, cols) {
+        this.rows = rows;
+        this.cols = cols;
+        this.data = [];
+
+        for (let r = 0; r < rows; r++) {
+            this.data.push(new Array(cols));
+            for (let c = 0; c < cols; c++) {
+                this.data[r][c] = 0;
+            }
+        }
+
+        //console.log(`The Matrix's data is ${this.data}`);
+    }
+}
+
+let canvas = new Matrix(3, 4);
+
 document.getElementById('root').innerHTML = 'Hello world!';
