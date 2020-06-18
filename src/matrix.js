0a5fecfeb3c0cfc4ae0e8db1fcf7d77ce254c876
John Pham <mr.john.pham@gmail.com>
2020-06-18 10:59:33 -0400

    Create matrix.js module

---
+++ src/matrix.js
@@ -0,0 +1,20 @@
+'use strict';
+
+class Matrix {
+    constructor(rows, cols) {
+        this.rows = rows;
+        this.cols = cols;
+        this.data = [];
+
+        for (let r = 0; r < rows; r++) {
+            this.data.push(new Array(cols));
+            for (let c = 0; c < cols; c++) {
+                this.data[r][c] = 0;
+            }
+        }
+
+        //console.log(`The Matrix's data is ${this.data}`);
+    }
+}
+
+export default Matrix;
